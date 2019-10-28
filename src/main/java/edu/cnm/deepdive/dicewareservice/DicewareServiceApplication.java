a1a140edd1af05c2cd56164aa48f4f03711d861e
edubois9119 <erica.dubois9119@gmail.com>
2019-10-28 14:09:10 -0600

    added Spring Security

--- src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
@@ -5,12 +5,26 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
 
 @SpringBootApplication
-public class DicewareServiceApplication {
+@EnableWebSecurity
+@EnableResourceServer
+public class DicewareServiceApplication extends ResourceServerConfigurerAdapter {
+
+  @Value("${oauth.clientId}")
+  private String clientId;
+
 
   public static void main(String[] args) {
     SpringApplication.run(DicewareServiceApplication.class, args);
@@ -26,4 +40,16 @@ public class DicewareServiceApplication {
     return ResourceBundle.getBundle("wordlist");
   }
 
+  @Override
+  public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    resources.resourceId(clientId);
+
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+ //   http.authorizeRequests().anyRequest().anonymous();
+    http.authorizeRequests().anyRequest().hasRole("USER");
+  }
 }
